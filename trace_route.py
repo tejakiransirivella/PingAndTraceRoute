0e9f440a750283b890de668fc720c0caa12b4ada
tejakiransirivella <tejakiran2013@gmail.com>
2023-10-01 23:04:19 -0400

    added options for traceroute

--- trace_route.py
+++ trace_route.py
@@ -1,24 +1,47 @@
 import socket
 import struct
+import sys
 import time
 
 from icmp_header import icmp_header
-from ip_header import ip_header
 
 
 class TraceRoute:
-    DEST_ADDRESS = '8.8.8.8'
-
-    __slots__ = "no_probes"
+    __slots__ = "no_probes", "numeric", "summary", "dest_address", "max_hops"
 
     def __init__(self):
         self.no_probes = 3
+        self.numeric = False
+        self.summary = False
+        self.dest_address = None
+        self.max_hops = 5
 
     def get_hostname_from_addr(self, addr):
-        try:
-            return socket.gethostbyaddr(addr)[0] + " ["+addr+"]"
-        except socket.error as err:
+        if self.numeric:
             return addr
+
+        try:
+            return socket.gethostbyaddr(addr)[0] + " [" + addr + "]"
+        except socket.error:
+            return addr
+
+    def handle_input(self):
+        arg_length = len(sys.argv)
+        for i in range(1, arg_length):
+            if sys.argv[i] == '-h':
+                i += 1
+            elif i == arg_length - 1:
+                self.dest_address = sys.argv[i]
+                i += 2
+            elif sys.argv[i] == '-n':
+                self.numeric = True
+                i += 1
+            elif sys.argv[i] == '-q':
+                self.no_probes = int(sys.argv[i+1])
+                i += 2
+            elif sys.argv[i] == '-S':
+                self.summary = True
+                i += 1
 
     def find_trace(self):
         try:
@@ -31,36 +54,57 @@ class TraceRoute:
             icmpheader.calculate_checksum()
 
             raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+            raw_socket.settimeout(4)
+            print("Tracing route to "+self.get_hostname_from_addr(self.dest_address))
+            print("over a maximum of " + str(self.max_hops) + " hops:\n")
 
             ttl_value = 1
             ip_addr = ""
-            while ip_addr != self.DEST_ADDRESS:
-                output = [ttl_value]
+            summary = []
+            while not (ttl_value > self.max_hops or ip_addr == self.dest_address):
+                output = [str(ttl_value).ljust(3)]
+                failed_probs = 0
                 for i in range(self.no_probes):
                     ttl_bytes = struct.pack('!B', ttl_value)
                     raw_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl_bytes)
+
                     send_time = time.time()
-                    raw_socket.sendto(bytes.fromhex(icmpheader.to_hex()), (self.DEST_ADDRESS, 0))
-                    data, addr = raw_socket.recvfrom(1024)
-                    recv_time = time.time()
-                    elapse_time = str(int((recv_time-send_time)*1000)) + " ms"
-                    output.append(elapse_time)
-                    if i == self.no_probes-1:
+                    try:
+                        raw_socket.sendto(bytes.fromhex(icmpheader.to_hex()), (self.dest_address, 0))
+                        data, addr = raw_socket.recvfrom(1024)
                         ip_addr = addr[0]
+                        recv_time = time.time()
+                        elapse_time = str(int((recv_time - send_time) * 1000)) + " ms"
+                        output.append(elapse_time.ljust(5))
+                    except socket.timeout:
+                        output.append("*".ljust(5))
+                        failed_probs += 1
+
+                    if i == self.no_probes - 1:
                         hostname_addr = self.get_hostname_from_addr(ip_addr)
                         output.append(hostname_addr)
+                summary.append([ttl_value, failed_probs])
+
                 for item in output:
                     print(item, end="      ")
                 print("")
                 ttl_value += 1
+
+            if self.summary:
+                print("\nhop no" + "     failed probes")
+                for record in summary:
+                    print(str(record[0]).ljust(12), record[1])
+
             raw_socket.close()
         except socket.error as err:
             print(err)
 
 
-def test():
+def main():
     trace_route = TraceRoute()
+    trace_route.handle_input()
     trace_route.find_trace()
 
 
-test()
+if __name__ == "__main__":
+    main()
